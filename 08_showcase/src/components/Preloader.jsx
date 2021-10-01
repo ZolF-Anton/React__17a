cc01ba07fc7b27f7362e2bc6987d0db431864b2e
ZolF-werk <zolf@bk.ru>
2021-10-01 16:31:47 +0300

    get_ask

--- 08_showcase/src/components/Preloader.jsx
+++ 08_showcase/src/components/Preloader.jsx
@@ -1,15 +1,15 @@
 function Preloader() {
     return (
-        <div className='preloader-wrapper big active'>
-            <div className='spinner-layer spinner-blue-only'>
-                <div className='circle-clipper left'>
-                    <div className='circle'></div>
+        <div className="preloader-wrapper big active centered_">
+            <div className="spinner-layer spinner-blue-only">
+                <div className="circle-clipper left">
+                    <div className="circle"></div>
                 </div>
-                <div className='gap-patch'>
-                    <div className='circle'></div>
+                <div className="gap-patch">
+                    <div className="circle"></div>
                 </div>
-                <div className='circle-clipper right'>
-                    <div className='circle'></div>
+                <div className="circle-clipper right">
+                    <div className="circle"></div>
                 </div>
             </div>
         </div>
